54b9f556f84a3326564db49b6a12278ca6d9d683
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2020-07-06 18:24:04 -0300

    Add project files.

---
+++ frmCuantoPoneCadaUno.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Cuanto_pone_cada_uno
+{
+  partial class frmCuantoPoneCadaUno
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // frmCuantoPoneCadaUno
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1612, 855);
+      this.Name = "frmCuantoPoneCadaUno";
+      this.Text = "Cuanto pone cada uno?";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
---
+++ frmCuantoPoneCadaUno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuanto_pone_cada_uno
+{
+  public partial class frmCuantoPoneCadaUno : Form
+  {
+    public frmCuantoPoneCadaUno()
+    {
+      InitializeComponent();
+    }
+  }
+}
